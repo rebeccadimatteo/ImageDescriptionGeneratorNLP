d1564667eee42679f709c15cbe636a07131512a5
rebeccadimatteo <beccadimatteoo@gmail.com>
2024-01-09 12:24:24 +0100

    Update DataCleaning

--- data_preparation/DataCleaning.py
+++ data_preparation/DataCleaning.py
@@ -2,7 +2,6 @@ import pandas as pd
 import random
 import os
 
-
 def data_cleaning():
     # Cartella delle immagini originali
     input_images_folder = '../input/images'
@@ -11,29 +10,22 @@ def data_cleaning():
     csv_path = '../input/dataset.csv'
 
     # Numero di immagini da eliminare casualmente
-    numero_immagini_da_eliminare = 30000  # Modifica questo numero in base alle tue esigenze
+    numero_immagini_da_eliminare = 15000  # Modifica questo numero in base alle tue esigenze
 
     # Carica il DataFrame dal file CSV
     df = pd.read_csv(csv_path, delimiter='|')
 
     df = df.drop(' comment_number', axis=1)
 
-    # Elimina le righe nulle
+    # Rimuovi le righe nulle dal DataFrame
     df = df.dropna()
 
-    # Rimuovi immagini associate alle righe nulle
-
-    for index, row in df.iterrows():
-        image_name = row['image_name']
-        image_path = os.path.join(input_images_folder, image_name)
-        if os.path.exists(image_path):
-            os.remove(image_path)
-
-    # Salva il nuovo DataFrame nel file CSV
-    df.to_csv('../processed/dataset.csv', index=False, sep='|')
-
     # Estrai un elenco casuale di nomi di immagini da eliminare
-    immagini_da_eliminare = random.sample(os.listdir(input_images_folder), numero_immagini_da_eliminare)
+    elenco_immagini = os.listdir(input_images_folder)
+    if len(elenco_immagini) > numero_immagini_da_eliminare:
+        immagini_da_eliminare = random.sample(elenco_immagini, numero_immagini_da_eliminare)
+    else:
+        immagini_da_eliminare = elenco_immagini
 
     # Elimina le immagini dalla cartella
     for img_name in immagini_da_eliminare:
@@ -43,6 +35,8 @@ def data_cleaning():
     # Filtra il DataFrame per mantenere solo le righe non associate alle immagini eliminate
     df = df[~df['image_name'].isin(immagini_da_eliminare)]
 
+    # Salva il nuovo DataFrame nel file CSV
+    df.to_csv('../processed/dataset.csv', index=False, sep='|')
 
 if __name__ == '__main__':
     data_cleaning()
