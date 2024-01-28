d3beecffb2b95704693caca98df2548636a7c9bf
Antonio La Marca <63302449+tonylm00@users.noreply.github.com>
2024-01-28 23:32:46 +0100

    Defined an initial model with an encoder-decoder structure

---
+++ data_preparation/preprocessing.py
@@ -0,0 +1,111 @@
+import os
+import re
+
+# Impostazione del backend di Keras su TensorFlow
+os.environ["KERAS_BACKEND"] = "tensorflow"
+import matplotlib
+
+# Utilizzo del backend TkAgg per Matplotlib
+matplotlib.use('TkAgg')
+import numpy as np
+import tensorflow as tf
+import keras
+
+# Impostazione di un seed per la riproducibilità
+keras.utils.set_random_seed(111)
+
+# Percorso delle immagini
+IMAGES_PATH = "input/images"
+
+# Dimensioni desiderate per le immagini
+IMAGE_SIZE = (299, 299)
+
+# Lunghezza fissa consentita per qualsiasi sequenza
+SEQ_LENGTH = 25
+
+
+def load_captions_data(filename):
+    # Carica i dati delle didascalie (testo) e li associa alle immagini corrispondenti.
+    with open(filename) as caption_file:
+        caption_data = caption_file.readlines()
+        caption_mapping = {}
+        text_data = []
+        images_to_skip = set()
+
+        for line in caption_data:
+            line = line.rstrip("\n")
+            # Nome immagine e didascalie sono separati da una tabulazione
+            img_name, caption = line.split("\t")
+
+            # Ogni immagine è ripetuta cinque volte per le cinque diverse didascalie.
+            # Ogni nome immagine ha un suffisso `#(numero_didascalia)`
+            img_name = img_name.split("#")[0]
+            img_name = os.path.join(IMAGES_PATH, img_name.strip())
+
+            # Rimuoviamo le didascalie troppo corte o troppo lunghe
+            tokens = caption.strip().split()
+
+            if len(tokens) < 5 or len(tokens) > SEQ_LENGTH:
+                images_to_skip.add(img_name)
+                continue
+
+            if img_name.endswith("jpg") and img_name not in images_to_skip:
+                # Aggiungiamo un token di inizio e uno di fine a ciascuna didascalia
+                caption = "<start> " + caption.strip() + " <end>"
+                text_data.append(caption)
+
+                if img_name in caption_mapping:
+                    caption_mapping[img_name].append(caption)
+                else:
+                    caption_mapping[img_name] = [caption]
+
+        for img_name in images_to_skip:
+            if img_name in caption_mapping:
+                del caption_mapping[img_name]
+
+        return caption_mapping, text_data
+
+
+def train_val_split(caption_data, train_size=0.8, shuffle=True):
+    # 1. Ottenere la lista di tutti i nomi delle immagini
+    all_images = list(caption_data.keys())
+
+    # 2. Mescolare se necessario
+    if shuffle:
+        np.random.shuffle(all_images)
+
+    # 3. Suddividere in set di allenamento e validazione
+    train_size = int(len(caption_data) * train_size)
+
+    training_data = {
+        img_name: caption_data[img_name] for img_name in all_images[:train_size]
+    }
+    validation_data = {
+        img_name: caption_data[img_name] for img_name in all_images[train_size:]
+    }
+
+    # 4. Restituire le divisioni
+    return training_data, validation_data
+
+
+def regex():
+    strip_chars = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+    strip_chars = strip_chars.replace("<", "")
+    strip_chars = strip_chars.replace(">", "")
+    return strip_chars
+
+
+def custom_standardization(input_string):
+    # Standardizzazione personalizzata del testo di input
+    lowercase = tf.strings.lower(input_string)
+    strip_chars = regex()
+    return tf.strings.regex_replace(lowercase, "[%s]" % re.escape(strip_chars), "")
+
+
+def decode_and_resize(img_path):
+    # Decodifica e ridimensiona l'immagine dal percorso specificato
+    img = tf.io.read_file(img_path)
+    img = tf.image.decode_jpeg(img, channels=3)
+    img = tf.image.resize(img, IMAGE_SIZE)
+    img = tf.image.convert_image_dtype(img, tf.float32)
+    return img
